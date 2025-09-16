ffbb1b0c9511696613769d7402fd99bb04d80fca
chiavariniluca <luca.chiavarini@hesplora.it>
2025-09-16 10:26:35 +0200

    ADD - datatables

--- WebApp/Simulatore/views.py
+++ WebApp/Simulatore/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, "home.html")
+    lista_simulazioni = [
+        {
+            'timestamp_esecuzione': '2025/08/10 17:00:00',
+            'stato': 'Schedulata',
+            'utente': 'Paolo Bianchi',
+            'nome_simulazione': 'Test 4'
+        },
+        {
+            'timestamp_esecuzione': '2025/07/23 15:00:00',
+            'stato': 'In lavorazione',
+            'utente': 'Mario Rossi',
+            'nome_simulazione': 'Test 3'
+        },
+        {
+            'timestamp_esecuzione': '2025/07/23 10:00:00',
+            'stato': 'Lavorata',
+            'utente': 'Mario Rossi',
+            'nome_simulazione': 'Test 2'
+        },
+        {
+            'timestamp_esecuzione': '2025/07/22 11:30:00',
+            'stato': 'Non completata',
+            'utente': 'Luca Neri',
+            'nome_simulazione': 'Test 1'
+        }
+    ]
+
+    context = {
+        'lista_simulazioni': lista_simulazioni
+    }
+    return render(request, "home.html", context)
 
 def calendario(request):
     return render(request, "calendario/calendario.html")
