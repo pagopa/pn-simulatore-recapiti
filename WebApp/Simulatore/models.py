0ae4f4873a7f4c8bc56f7e4c9586827c681c9d02
chiavariniluca <luca.chiavarini@hesplora.it>
2025-10-07 16:03:55 +0200

    EDIT - models

--- WebApp/Simulatore/models.py
+++ WebApp/Simulatore/models.py
@@ -6,8 +6,8 @@ class CustomUser(AbstractUser):
 
 class table_simulazione(models.Model):
     ID = models.AutoField(primary_key=True, unique=True)
-    NOME = models.CharField(max_length=50)
-    DESCRIZIONE = models.TextField()
+    NOME = models.CharField(max_length=50, null=True)
+    DESCRIZIONE = models.TextField(null=True)
     UTENTE_ID = models.ForeignKey(CustomUser, db_column='UTENTE_ID', on_delete=models.CASCADE, null=True)
     CHOICES_STATO = [
         ("Lavorata", "Lavorata"),
@@ -16,8 +16,8 @@ class table_simulazione(models.Model):
         ("Non completata", "Non completata"),
         ("Bozza", "Bozza")
     ]
-    STATO = models.CharField(max_length=20, choices=CHOICES_STATO)
-    TIMESTAMP_ESECUZIONE = models.DateTimeField()
+    STATO = models.CharField(max_length=20, choices=CHOICES_STATO, null=True)
+    TIMESTAMP_ESECUZIONE = models.DateTimeField(null=True)
     class Meta:
         db_table = 'SIMULAZIONE'
         indexes = [
@@ -26,13 +26,15 @@ class table_simulazione(models.Model):
 
 class table_capacita_modificate(models.Model):
     ID = models.AutoField(primary_key=True, unique=True)
-    MESE_SIMULAZIONE = models.CharField(max_length=20)
-    TIPO_CAPACITA = models.CharField(max_length=20)
-    RECAPITISTA = models.CharField(max_length=20)
-    GEOKEY = models.CharField(max_length=20)
-    CAPACITA = models.CharField(max_length=20)
-    ACTIVATION_DATE_FROM = models.DateField()
-    ACTIVATION_DATE_TO = models.DateField()
-    SIMULAZIONE_ID = models.ForeignKey(table_simulazione, db_column='SIMULAZIONE_ID', on_delete=models.CASCADE)
+    MESE_SIMULAZIONE = models.CharField(max_length=20, null=True)
+    TIPO_CAPACITA = models.CharField(max_length=25, null=True)
+    RECAPITISTA = models.CharField(max_length=50, null=True)
+    REGIONE = models.CharField(max_length=50, null=True)
+    PROVINCIA = models.CharField(max_length=50, null=True)
+    POSTALIZZAZIONI = models.IntegerField(null=True)
+    CAPACITA = models.IntegerField(null=True)
+    ACTIVATION_DATE_FROM = models.DateField(null=True)
+    ACTIVATION_DATE_TO = models.DateField(null=True)
+    SIMULAZIONE_ID = models.ForeignKey(table_simulazione, db_column='SIMULAZIONE_ID', on_delete=models.CASCADE, null=True)
     class Meta:
         db_table = 'CAPACITA_MODIFICATE'
