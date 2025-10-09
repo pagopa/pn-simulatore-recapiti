ff3f2064e1316ef6e07ec6a724dcf54f83e0d264
chiavariniluca <luca.chiavarini@hesplora.it>
2025-10-09 09:47:37 +0200

    EDIT - views: rimosse librerie inutilizzate

--- WebApp/Simulatore/views.py
+++ WebApp/Simulatore/views.py
@@ -13,8 +13,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
-from datetime import datetime, timezone, timedelta
-import pytz
+from datetime import datetime
 
 from .models import *
 
