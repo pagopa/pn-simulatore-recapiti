60a5aebcffbc648dcec809ca863c471a2cf1d7db
chiavariniluca <luca.chiavarini@hesplora.it>
2026-07-13 14:59:38 +0200

    Fase 2 - RF2 + Fix id_simulazione_manuale

--- functions/ConfigurazioneSimulazione/lambda_function.py
+++ functions/ConfigurazioneSimulazione/lambda_function.py
@@ -6,7 +6,6 @@ Trigger:
 
 Input:
     tipo_simulazione: 'Automatizzata' o 'Manuale'
-    id_simulazione_manuale: valorizzata con l'id di simulazione in caso di simulazione 'Manuale', vuota per simulazione 'Automatizzata'
     mese_simulazione: prima settimana del mese di simulazione, nel formato yyyy-MM-dd
 
 Output:
