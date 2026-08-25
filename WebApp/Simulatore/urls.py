f04490192f8200e295476c0087915cb5bc62ab37
chiavariniluca <luca.chiavarini@hesplora.it>
2026-08-25 16:22:50 +0200

    pulizia branch

--- WebApp/Simulatore/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from Simulatore.views import *
-
-urlpatterns = [
-    path('risultati/<id_simulazione>', risultati, name='risultati'),
-    path('confronto_risultati/<int:id1>/<int:id2>/', confronto_risultati, name='confronto_risultati'),
-]
